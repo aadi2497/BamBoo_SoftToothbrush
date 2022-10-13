04402387c9e954c136226e7ab7cb53b8dea1c2bb
w0751432 <w0751432@ADSOL0171B-04>
2022-10-13 13:16:02 -0400

    Add project files.

---
+++ BamBoo_SoftToothbrush/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BamBoo_SoftToothbrush.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
