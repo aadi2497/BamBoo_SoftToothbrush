b8fb19c46ea0d11f0da81da580b85518f168c89f
aadi2497 <w0751432@myscc.ca>
2022-10-13 22:24:05 -0400

    Program File Changes to run SeedData

--- BamBoo_SoftToothbrush/Program.cs
+++ BamBoo_SoftToothbrush/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using BamBoo_SoftToothbrush.Data;
+using BamBoo_SoftToothbrush.Models;
 
 namespace BamBoo_SoftToothbrush
 {
@@ -13,7 +16,25 @@ namespace BamBoo_SoftToothbrush
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
